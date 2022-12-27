6978cb526524cf9211aa5e3cc13efc109e098c60
Angel Haro <haro.j.angel@gmail.com>
2022-12-26 23:44:58 -0800

    .

--- Main/Main.md
+++ Main/Main.md
@@ -1,2 +1,31 @@
-![](z/Screenshot%202022-12-26%20at%202.11.18%20PM.png)
-![](z/Screenshot%202022-12-26%20at%202.39.39%20PM.png)
+
+Lets try again
+
+![](Screenshot%202022-12-26%20at%2011.32.18%20PM.png)
+
+
+
+
+
+Another Picture below 
+
+![](Screenshot%202022-12-26%20at%2011.32.43%20PM.png)
+
+
+
+
+
+
+
+
+<!-- ![](z/Screenshot 2022-12-26 at 11.32.18 PM.png)
+![](z/Screenshot 2022-12-26 at 11.32.43 PM.png)
+
+
+![](z/Screenshot%202022-12-26%20at%2011.32.18%20PM.png)
+![](z/Screenshot%202022-12-26%20at%2011.32.43%20PM.png) -->![](z/Screenshot%202022-12-26%20at%2011.33.17%20PM.png)
+![](z/Screenshot%202022-12-26%20at%2011.32.18%20PM.png)
+![](z/Screenshot%202022-12-26%20at%2011.32.43%20PM.png)
+![](z/Screenshot%202022-12-26%20at%2011.33.17%20PM.png)
+![](z/Screenshot%202022-12-26%20at%2011.32.18%20PM.png)
+![](z/Screenshot%202022-12-26%20at%2011.32.43%20PM.png)
--- Main/Main.py
+++ Main/Main.py
@@ -1,7 +1,15 @@
-# Open the Markdown file in write mode
-with open('Main.md', 'w') as f:
-  # Write the image tag with the file path to the file
-  f.write('![](z/Screenshot%202022-12-26%20at%202.11.18%20PM.png)')
-  f.write('\n')  # Add a newline character
-  f.write('![](z/Screenshot%202022-12-26%20at%202.39.39%20PM.png)')
+import os
+import urllib.parse
+
+# Get a list of all the files in the 'z' directory
+image_paths = [f for f in os.listdir('z') if os.path.isfile(os.path.join('z', f))]
+
+# Open the Markdown file in append mode
+with open('Main.md', 'a') as f:
+  # Iterate over the list of file paths and write an image tag for each path to the file
+  for path in image_paths:
+    # Encode the file path using the quote() function
+    encoded_path = urllib.parse.quote(path)
+    f.write('![](z/%s)' % encoded_path)
+    f.write('\n')  # Add a newline character
   # The file is automatically closed when the block ends
